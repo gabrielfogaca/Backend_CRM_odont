bdc3c8652f93c91e2a652d6bffbb7dd05566c2d2
gabrielfogaca <gabriel.fogaca@unijui.edu.br>
2023-04-18 19:22:52 -0300

    ajustes

--- conection.js
+++ conection.js
@@ -1,13 +1,5 @@
-// const express = require('express');
 const mysql = require('mysql2/promise');
 require('dotenv').config();
-
-console.log(
-  process.env.DB_HOST,
-  process.env.DB_USERNAME,
-  process.env.PASSWORD,
-  process.env.DBNAME,
-);
 
 const config = {
   host: process.env.DB_HOST,
